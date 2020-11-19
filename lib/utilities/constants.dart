0e1f1ee63064c2b56bd147866993d5193b1c78c2
Bryan Bombeo <72188595+bryan100-sudo@users.noreply.github.com>
2020-11-19 20:38:12 +0800

    Add files via upload

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF757575),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
